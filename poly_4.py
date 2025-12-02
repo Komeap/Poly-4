87a978f00eafef31c47a3b0eb20ad197439a329c
Komeap <komeap.hynagame@gmail.com>
2025-12-02 17:51:33 +0100

    Ajout checking avec bitboard

--- poly_4.py
+++ poly_4.py
@@ -7,12 +7,17 @@ class Plateau:
     ## @param colones Nombre de colones du plateau
     ## @param win conditon du tableau, nbr de jeton qu'il faut alligner
     def __init__(self, lignes, colones, win_conditon):
-        self.l = lignes
+        self.l = lignes ## nombre de lignes <=> hauteur d'uen colone
         self.c = colones
         self.win = win_conditon
         self.matrice = np.zeros((lignes, colones), dtype = int)
         self.fill_matrice = np.zeros((colones), dtype = int)
+        self.bitboards = [0, 0] ## @brief bitboards[0] -> celle de l'ordi, bitboards[1]-> celle du joueur
+        ## Une bitboard est un nombre binaire, qui représente le plateau de jeu "applatie", avec 1 un jeton et 0 jetons enemmie, ou rien
 
+    def add_coup_bitboard(self, pos):
+        hauteur = self.l + 1 ## ajout d'un zero a la fin de chaque colone en guise de "mur" 
+        self.bitboards[pos[2]] = self.bitboards[pos[2]] | (1 << (pos[1] + hauteur*pos[0]) )
 
     ## @brief Permet l'affichage du plateau et de ca matrice de remplissage qui indique le nombre de jeton jouer sur cette colonnes
     def print_tab(self):
@@ -22,61 +27,54 @@ class Plateau:
 
     ## @brief Joue sur la colone play_colone
     ## @param joueur 1 ou 2 celon le joueur (Peut etre d'autre chiffre celon les bonus, ...)
-    ## @return !!!!!! Return 1 si ca peut pas jouer, et 0 si c'est ok et ca joue
+    ## @return !!!!!! Return 1 si ca peut pas jouer, et return (ligne, colone, num du joueur) si c'est ok et ca joue !!!!!!!
     def play(self, play_colone, joueur):
         ## @breif joue en [(hauteur de la matrice)-(le nombre de jeton sur cette colone)][colone ou c'est jouer]
-        if (self.fill_matrice[play_colone] < self.l ) :
-            self.matrice[(self.l - 1) - self.fill_matrice[play_colone]][play_colone] = joueur
-            ## @brief modifie la matrice de remplicage en ajoutant le coup jouer 
-            self.fill_matrice[play_colone] += 1
-            return 0
-        else :
-            return 1
+        if joueur == 2 : j = 0 
+        else : j = joueur
+        if (play_colone < self.c and play_colone >= 0):
+            if (self.fill_matrice[play_colone] < self.l) :
+                self.matrice[(self.l - 1) - self.fill_matrice[play_colone]][play_colone] = joueur
+                ## @brief modifie la matrice de remplicage en ajoutant le coup jouer 
+                self.fill_matrice[play_colone] += 1
+                print("Le coup est en (", self.fill_matrice[play_colone] - 1,",", play_colone,") du joueur :", joueur)
+                
+                self.add_coup_bitboard((self.fill_matrice[play_colone] - 1 , play_colone, j)) ## modifi la bitbord du joeuur en même temps
+                return (self.fill_matrice[play_colone] - 1 , play_colone, joueur)
+        return 1
 
 
-    ## @brief Verifie si le joueur a gagner 
-    ## @return (booleen win, le joueur qui a gagner) -> (0,-1): pas de gagant // (1, 1): le joueur 1 a gagner // (1, 2): le joueur 2 a gagner
-    def check_win(self):
-        ## @brief Check la conditon de victoire sur les colones
-        for i in range(self.c) :
-            nbr_a_la_suite = 0
-            nbr_rechercher = -1
-            for j in range(self.l) :
-                #print("Je check la case ",j," ",i)
-                if nbr_rechercher == -1 or nbr_rechercher == 0:
-                        nbr_rechercher = self.matrice[j][i]
-                if nbr_rechercher == self.matrice[j][i] and self.matrice[j][i] != 0:
-                    nbr_a_la_suite += 1
-                elif nbr_rechercher != self.matrice[j][i]:
-                    nbr_rechercher = self.matrice[j][i]
-                    nbr_a_la_suite = 1
-                if nbr_a_la_suite == self.win :
-                    return (1, nbr_rechercher)
+    def check_win_bitboard(self, joueur):
+        ## @brief chack vers le bas
+        H = self.l +1
+        directions = [1, H, H+1, H-1 ]
+        for d in directions:
+            bb = self.bitboards[joueur]
+            for i in range(self.win - 1):
+                bb = bb & (bb >> d)
+
+            if bb != 0:
+                return True
+        return False
+
+
+            
         
-        ## @brief Check la conditon de victoire sur les lignes
-        for j in range(self.l) :
-            nbr_a_la_suite = 0
-            nbr_rechercher = -1
-            for i in range(self.c) :
-                #print("Je check la case ",j," ",i)
-                if nbr_rechercher == -1 or nbr_rechercher == 0:
-                        nbr_rechercher = self.matrice[j][i]
-                if nbr_rechercher == self.matrice[j][i] and self.matrice[j][i] != 0:
-                    nbr_a_la_suite += 1
-                elif nbr_rechercher != self.matrice[j][i]:
-                    nbr_rechercher = self.matrice[j][i]
-                    nbr_a_la_suite = 1
-                if nbr_a_la_suite == self.win :
-                    return (1, nbr_rechercher)
-                    
-        ## Faire le check en diagonale
-        return (0, -1)
-
-
-
-game = Plateau(4,4,3)
+game = Plateau(5,4,4)
 joueur = 1
-while game.check_win()[0] == 0:
+while not(game.check_win_bitboard(1)):
+    print(game.matrice)
+    next = int(input())
+    game.play(next, joueur)
+    if joueur == 1:
+        joueur = 2
+    else : joueur = 1
+print(game.matrice)
+
+
+
+
+"""while game.check_win()[0] == 0:
     print(game.matrice)
     next = int(input())
     game.play(next, joueur)
@@ -89,5 +87,5 @@ if game.check_win()[0] == 0:
     print("Pas de gagnant")
 else : 
     print("Le joueur", game.check_win()[1], "a gagner")
-game.print_tab()
+game.print_tab()"""
 
